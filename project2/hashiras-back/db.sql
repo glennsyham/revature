da8f7b74a9dbb3aff50b7406817e3a55cf34985f
Glenn Syham <glennsyham@gmail.com>
2022-08-04 07:20:05 -0700

    sql file

---
+++ project2/hashiras-back/db.sql
@@ -0,0 +1,26 @@
+CREATE TABLE public."comments" (
+	comment_id SERIAL  PRIMARY KEY,
+	anime_id integer NULL,
+	"comment" varchar(255) NULL,
+	author integer references users(id)
+);
+ 
+
+CREATE TABLE public.lists (
+	id SERIAL  PRIMARY KEY,
+	anime_id integer NULL,
+ 	user_rating integer NULL,
+	status varchar(30) NULL,
+    user_id integer references users(id)
+);
+
+
+CREATE TABLE public.users (
+	id SERIAL  PRIMARY KEY,
+	username varchar(30) NULL,
+	"password" varchar(100) NULL,
+	fname varchar(30) NULL,
+	lname varchar(30) NULL,
+	email varchar(100) NULL,
+	status integer NULL,
+);
